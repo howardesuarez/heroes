bdcd3f0829378f260f908885a1379cef47169a6c
howardesuarez <howardesuarez@gmail.com>
2020-12-13 16:43:23 -0500

    setting up call and menu methods for cli

--- lib/cli.rb
+++ lib/cli.rb
@@ -1,5 +1,21 @@
 class Heroes::CLI
     def call
-        
+        puts "You have now activated the Heroes database."
+        menu
+
     end
+
+    def menu
+
+        puts "Who are you looking for?"
+       
+        @name = get.strip.downcase
+        new_hero = API.get_hero(@name)
+        
+        puts "What do you need to know about em? Choose below"
+        
+        array = ["BIO","LOOKS","CREW"]
+
+    end
+
 end
