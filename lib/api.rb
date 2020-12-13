c9110ca7413651d9cb57bddf1c6e8e9b25897536
howardesuarez <howardesuarez@gmail.com>
2020-12-13 15:56:35 -0500

    setting up api

--- lib/api.rb
+++ lib/api.rb
@@ -2,6 +2,8 @@ require_relative '../confi/environment.rb'
 
 class Heroes::API
 
-    def self.get_hero
-        @url ="https://superheroapi.com/api/4206379799379271"
+    def self.get_hero(name)
+        @url ="https://superheroapi.com/api/4206379799379271/search/#{name}"
+        uri = URI.parse(@url)
+        response = Net::HTTP.get(uri)
 end
