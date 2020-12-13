3f5a727addb03ef092fd9eab1ecf3fdc582c6d25
howardesuarez <howardesuarez@gmail.com>
2020-12-13 16:17:47 -0500

    using api to parse information

--- lib/api.rb
+++ lib/api.rb
@@ -3,7 +3,27 @@ require_relative '../confi/environment.rb'
 class Heroes::API
 
     def self.get_hero(name)
-        @url ="https://superheroapi.com/api/4206379799379271/search/#{name}"
-        uri = URI.parse(@url)
-        response = Net::HTTP.get(uri)
+        url ="https://superheroapi.com/api/4206379799379271/search/#{name}"
+        response = Net::HTTP.get(URI(url))
+        object = JSON.parse(response)
+        @results = object["results"]
+
+        cli = CLI.new
+            if object["response"] == "error"
+             puts "This Hero is not in our database, apologies."   
+            cli.replay
+         else
+     end
+
+        bio = @results[0]["biography"]
+        appearance = @results[0]["appearance"]
+        connections = @results[0]["connections"]
+        Hero.new(name,bio,appearance,connecions)
+
+    end
+
+
+
+
+
 end
